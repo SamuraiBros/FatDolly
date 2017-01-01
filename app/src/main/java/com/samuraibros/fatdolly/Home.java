9250c490324d14f31e33b2071744c5d0026e3008
africanwarrrior <africanwarrrior@gmail.com>
2016-12-31 23:31:37 -0500

    Basic discover device implementation: Basic application specific discovery with device names; Fixed error with not requesting device name

--- app/src/main/java/com/samuraibros/fatdolly/Home.java
+++ app/src/main/java/com/samuraibros/fatdolly/Home.java
@@ -17,7 +17,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //TEST
+        checkName();
     }
 
     /**
